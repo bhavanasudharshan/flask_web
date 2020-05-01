88ef810174f05cde97a5767eebda2dbb5c2a4d3d
Bhavana Sudharshan <bsudharshan@Bharaths-MacBook-Pro-2.local>
2020-04-30 18:06:36 -0700

    fix redis connection

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 # flask_web/app.py
 from db.mongo_db import conn
 from flask import Flask
+import redis
+
 app = Flask(__name__)
+
+redis_host = "localhost"
+redis_port = 6379
+redis_password = ""
 
 @app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
+
+@app.route('/testredis')
+def redis_test():
+    try:
+
+        # The decode_repsonses flag here directs the client to convert the responses from Redis into Python strings
+        # using the default encoding utf-8.  This is client specific.
+        r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+
+        # step 4: Set the hello message in Redis
+        r.set("msg:hello", "Hello Redis!!!")
+
+        # step 5: Retrieve the hello message from Redis
+        msg = r.get("msg:hello")
+        return msg
+
+    except Exception as e:
+        print(e)
+
 
 
 @app.route('/testconn')
