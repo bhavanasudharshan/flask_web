07e86ffb2f021c8a846e24d2ca7da4c990e5e370
Bhavana Sudharshan <bsudharshan@Bharaths-MacBook-Pro-2.local>
2020-06-03 13:10:19 -0700

    fixing of the celery module launch error

--- app.py
+++ app.py
@@ -1,32 +1,58 @@
 # flask_web/app.py
 import json
+from concurrent.futures.thread import ThreadPoolExecutor
 from time import strftime, gmtime
 
 import pika
 import redis
+from celery import Celery
 from flask import Flask, request
 from pymongo import MongoClient
+
+try:
+    from .background_task import send_to_rabbitMQ
+except ImportError:
+    from background_task import send_to_rabbitMQ
 try:
     from .rediscli import get_cache
 except ImportError:
     from rediscli import get_cache
 
-try:
-    from .tasks import createMapperJobs
-except ImportError:
-    from tasks import createMapperJobs
 
-app = Flask(__name__)
-app.config['enable-threads'] = True
+def make_celery(app):
+    celery = Celery(
+        'worker',
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+
+flask_app = Flask(__name__)
+flask_app.config['enable-threads'] = True
+flask_app.config['CELERY_BROKER_URL'] = 'amqp://guest@rabbitmq//'
+flask_app.config['CELERY_RESULT_BACKEND'] = 'amqp://guest@rabbitmq//'
+
+celery = make_celery(flask_app)
+
 db_client = MongoClient(host="mongodb")
+executor = ThreadPoolExecutor(max_workers=3)
 
 
-@app.route('/')
+@flask_app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
 
 
-@app.route('/testredis')
+@flask_app.route('/testredis')
 def redis_test():
     try:
         get_cache().set("msg:hello", "Hello Redis!!!")
@@ -36,7 +62,7 @@ def redis_test():
         raise exc
 
 
-@app.route('/testconn')
+@flask_app.route('/testconn')
 def test_conn():
     db_client = MongoClient(host="mongodb")
     var = db_client["crm"]
@@ -45,7 +71,7 @@ def test_conn():
     return var.name
 
 
-@app.route('/wordcount/<word>', methods=["GET"])
+@flask_app.route('/wordcount/<word>', methods=["GET"])
 def word_count(word):
     cache_result = get_cache().get(word)
     if cache_result is not None:
@@ -60,7 +86,7 @@ def word_count(word):
         return json.dumps({'word': word, 'count': doc['value']})
 
 
-@app.route('/send', methods=["POST"])
+@flask_app.route('/send', methods=["POST"])
 def rmq_send():
     req_data = request.form
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
@@ -74,7 +100,7 @@ def rmq_send():
         'started. rabbitmq message sent': True})
 
 
-@app.route('/recieve/<userId>', methods=["GET"])
+@flask_app.route('/recieve/<userId>', methods=["GET"])
 def rmq_recieve(userId):
     if get_cache().get(userId) is None:
         return "found nothing in queue"
@@ -82,7 +108,99 @@ def rmq_recieve(userId):
         return get_cache().get(userId)
 
 
-@app.route('/wordcountstart', methods=["POST"])
+@celery.task(name='app.createMapperJobs')
+def createMapperJobs():
+    file = "/app/words.txt"
+    for i in range(0, 1000):
+        lines = []
+        with open(file=file) as inputfile:
+            for line in inputfile:
+                lines.append(line)
+            print("creating a mapper job {}")
+            print(json.dumps(lines))
+            get_cache().incr("COUNT")
+            send_to_rabbitMQ(json.dumps(lines), "mapper")
+
+    # start the mapper consumer threads once the total jobs are tracked
+    executor.submit(threaded_rmq_mapper_task)
+    executor.submit(threaded_rmq_mapper_task)
+    executor.submit(threaded_rmq_mapper_task)
+
+
+@celery.task(name='app.reducer')
+def reducer(key):
+    print("starting reducer job {0}".format(key))
+    mydb = db_client["crm"]
+    results_collection = mydb["results"]
+    reducer_entry = mydb["reducerKeys"].find_one({'key': key})
+
+    count = 0
+
+    for c in reducer_entry['value']:
+        print("count incrementing for reducer {0}".format(key))
+        count = count + c
+    results_collection.update({"key": key}, {'$set': {"value": count}}, upsert=True)
+    print("reducer finishing for key {0} output {1}".format(key, c))
+
+
+@celery.task(name='app.shuffle')
+def shuffle():
+    reducer_job_keys = {}
+
+    mydb = db_client["crm"]
+    reducer_collection = mydb["reducerKeys"]
+
+    mapper_output = mydb["mapperOutput"]
+    for item in mapper_output.find():
+        print(item)
+        if item['key'] not in reducer_job_keys:
+            reducer_job_keys[(item['key'])] = True
+        reducer_collection.update_one({"key": item['key']}, {"$push": {"value": item['value']}}, upsert=True)
+
+    for key in reducer_job_keys:
+        reducer.delay(key)
+
+
+def wordMapper(arr):
+    print("in word mapper")
+    mydb = db_client["crm"]
+    mapperOutput = mydb["mapperOutput"]
+    for item in arr:
+        item = item.strip()
+        mapperOutput.insert_one({'key': item, 'value': 1})
+        print('inserting into mapperOutput {0}'.format(item))
+
+    get_cache().decr("COUNT")
+    count = get_cache().get("COUNT")
+
+    if count == "0":
+        print("finished processing all mapper jobs, shuffler task starting")
+        shuffle.delay()
+    else:
+        print("remaining mapper jobs to be processed {0} {1} {2}".format(count, count == "0", count == 0))
+
+
+def mapper_callback(ch, method, properties, body):
+    print(" [x] Received mapper %r" % body)
+    print(body.decode('utf8'))
+    wordMapper(json.loads(body.decode('utf8')))
+
+
+def threaded_rmq_mapper_task():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='mapper')
+
+    channel.basic_consume(
+        queue='mapper', on_message_callback=mapper_callback, auto_ack=True)
+
+    print(' [*] Waiting for mapper messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+@flask_app.route('/wordcountstart', methods=["POST"])
 def word_count_start():
     if get_cache().get("COUNT") != "0":
         if 'force' not in request.form or request.form['force'] != "true":
@@ -100,4 +218,4 @@ def word_count_start():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    flask_app.run(debug=False, host='0.0.0.0')
